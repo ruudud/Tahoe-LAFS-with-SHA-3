11f16fda2ba61e5a1d79f48c75af46e8d3961631
Eirik Haver <eiriha@gmail.com>
2010-09-13 15:12:43 +0200

    working bmw256 class?

--- python-bindings/bmw.pyx
+++ python-bindings/bmw.pyx
@@ -16,3 +16,47 @@ def hash(int hashbitlen, unsigned char[] data):
         free(hashval)
 
     return ret
+
+
+cdef class bmw256:
+    cdef bmw_h.hashState state
+    cdef bmw_h.hashState previous
+    cdef int hashbitlen
+    cdef bmw_h.BitSequence *hashval
+    cdef int finished
+
+    def __init__(self, msg=None):
+        self.hashbitlen = 256
+        bmw_h.Init(&self.state,self.hashbitlen)
+        self.finished = 0
+        if msg:
+            self.update(msg)
+
+        return
+    
+    cpdef update(self, unsigned char data[]):
+        if self.finished:
+            self.finished = 0
+            self.state = self.previous
+        bmw_h.Update(&self.state,data,len(data)*8)
+        return
+    
+    cpdef final(self):
+        self.hashval = <bmw_h.BitSequence *> malloc(self.hashbitlen*8)
+        self.previous = self.state
+        bmw_h.Final(&self.state, self.hashval)
+        self.finished = 1
+        return
+    
+    def hexdigest(self):
+        if not self.finished:
+            self.final()
+        ret = [self.hashval[i] for i from 0 <= i < self.hashbitlen / 8]
+        return [hex(i) for i in ret]
+
+    cpdef copy(self):
+        s = bmw256()
+        s.state = self.state
+        s.previous = self.previous
+        s.finished = self.finished
+        return s
--- python-bindings/bmw_h.pxd
+++ python-bindings/bmw_h.pxd
@@ -8,4 +8,16 @@ cdef extern from "BlueMidnightWish.h":
         BAD_HASHLEN = 2
         BAD_CONSECUTIVE_CALL_TO_UPDATE = 3 
 
+    ctypedef struct Data256:
+        pass
+
+    ctypedef struct Data512:
+        pass 
+
+    ctypedef struct hashState:
+        pass
+
     HashReturn Hash(int, BitSequence *data, DataLength, BitSequence *hashval)
+    HashReturn Init(hashState *state, int)
+    HashReturn Update(hashState *state, BitSequence *data, DataLength)
+    HashReturn Final(hashState *state, BitSequence *hashval)
