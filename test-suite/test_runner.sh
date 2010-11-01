a90b35bb7eec6426014db63363467b26f9fa5a29
Pål Ruud <ruudud@gmail.com>
2010-11-01 17:03:57 +0100

    Updated with tahoe commands

--- test-suite/test_runner.sh
+++ test-suite/test_runner.sh
@@ -6,16 +6,24 @@
 file_types=(1b 1kb 1mb 100mb 1gb)
 
 for ftype in ${file_types[*]}; do
-    files=`ls $ftype*`
     mkdir /tmp/$ftype
+    echo '' > /tmp/time_spent_hashing_put
+    echo '' > /tmp/time_spent_hashing_get
+
+    files=`ls $ftype*`
 
     for file in $files; do
-        #tahoe put $file
+        #tahoe cp $file tahoe:
         echo $file
     done
 
+    cp /tmp/time_spent_hashing_put ./$ftype\_tshp.txt
+
     for file in $files; do
-        #tahoe cp tahoe:$file /tmp/$ftype/$file
+        #tahoe get $file > /dev/null
+        #tahoe cp $file /tmp/$ftype/$file
         echo $file
     done
+
+    cp /tmp/time_spent_hashing ./$ftype\_tshg.txt
 done
