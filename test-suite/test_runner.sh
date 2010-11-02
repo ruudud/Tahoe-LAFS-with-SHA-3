fe4de0d512747ca4ddf6063296c1883cf5f401f9
Pål Ruud <ruudud@gmail.com>
2010-11-02 16:18:34 +0100

    Added number of hashops count and commented in tahoe cmds

--- test-suite/test_runner.sh
+++ test-suite/test_runner.sh
@@ -9,21 +9,23 @@ for ftype in ${file_types[*]}; do
     mkdir /tmp/$ftype
     echo '' > /tmp/time_spent_hashing_put
     echo '' > /tmp/time_spent_hashing_get
+    echo '0' > /tmp/number_of_hashops
 
     files=`ls $ftype*`
 
     for file in $files; do
-        #tahoe cp $file tahoe:
-        echo $file
+        tahoe cp $file tahoe:
     done
 
     cp /tmp/time_spent_hashing_put ./$ftype\_tshp.txt
+    cp /tmp/number_of_hashops ./$ftype\_nohashopsp.txt
+    echo '0' > /tmp/number_of_hashops
 
     for file in $files; do
-        #tahoe get $file > /dev/null
-        #tahoe cp $file /tmp/$ftype/$file
-        echo $file
+        tahoe get $file > /dev/null
+        tahoe cp $file /tmp/$ftype/$file
     done
 
     cp /tmp/time_spent_hashing ./$ftype\_tshg.txt
+    cp /tmp/number_of_hashops ./$ftype\_nohashopsg.txt
 done
