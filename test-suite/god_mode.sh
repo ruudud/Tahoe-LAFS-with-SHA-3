b655ec7ca260f1100fe17380b5424b7d9ca832f0
Eirik Haver <eiriha@gmail.com>
2010-11-03 14:34:18 +0100

    sha256 in god mode

--- test-suite/god_mode.sh
+++ test-suite/god_mode.sh
@@ -5,7 +5,7 @@
 #   Starte noder osv. 
 #   Starte testing
 
-for i in blake  bmw  cubehash  echo  fugue  groestl  hamsi \
+for i in sha256 blake  bmw  cubehash  echo  fugue  groestl  hamsi \
     jh  keccak  luffa  shabal  shavite3  simd  skein;
 do
     /home/eiriha/tahoe-svn/scripts/do_it_all.sh $i
