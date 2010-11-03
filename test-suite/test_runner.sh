c9667da989213711fa76ef7695ba2b9e67ebc6c4
Pål Ruud <ruudud@gmail.com>
2010-11-03 11:25:41 +0100

    Updated flat filenames

--- test-suite/test_runner.sh
+++ test-suite/test_runner.sh
@@ -6,9 +6,7 @@
 file_types=(1b 1kb 1mb 100mb 1gb)
 
 for ftype in ${file_types[*]}; do
-    mkdir /tmp/$ftype
-    echo '' > /tmp/time_spent_hashing_put
-    echo '' > /tmp/time_spent_hashing_get
+    echo '0' > /tmp/time_spent_hashing
     echo '0' > /tmp/number_of_hashops
 
     files=`ls $ftype*`
@@ -17,13 +15,14 @@ for ftype in ${file_types[*]}; do
         tahoe cp $file tahoe:
     done
 
-    cp /tmp/time_spent_hashing_put ./$ftype\_tshp.txt
+    cp /tmp/time_spent_hashing ./$ftype\_tshp.txt
     cp /tmp/number_of_hashops ./$ftype\_nohashopsp.txt
+    echo '0' > /tmp/time_spent_hashing
     echo '0' > /tmp/number_of_hashops
 
     for file in $files; do
-        tahoe get $file > /dev/null
-        tahoe cp $file /tmp/$ftype/$file
+        #tahoe get $file > /dev/null
+        tahoe cp tahoe:$file /tmp/$ftype/$file
     done
 
     cp /tmp/time_spent_hashing ./$ftype\_tshg.txt
