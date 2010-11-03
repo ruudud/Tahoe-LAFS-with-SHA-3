7eb40d89dad16311ac77f8c1c6fb1f991c836c21
Pål Ruud <ruudud@gmail.com>
2010-11-03 14:31:57 +0100

    Added timing

--- test-suite/test_runner.sh
+++ test-suite/test_runner.sh
@@ -14,24 +14,30 @@ for ftype in ${file_types[*]}; do
     files=`ls $ftype*`
     total=`ls $ftype* | wc -l`
 
+    time1=`date +%s.%N`
     counter=1
     for file in $files; do
         tahoe cp $file tahoe: > /dev/null
         echo $ftype '-put: ' $counter '/' $total
         let counter=counter+1
     done
+    time2=`date +%s.%N`
+    echo $time2 - $time1 | bc > ./logs/$ftype\_totaltimeput.txt
 
     cp /tmp/time_spent_hashing ./logs/$ftype\_tshp.txt
     cp /tmp/number_of_hashops ./logs/$ftype\_nohashopsp.txt
     echo '0' > /tmp/time_spent_hashing
     echo '0' > /tmp/number_of_hashops
 
+    time1=`date +%s.%N`
     counter=1
     for file in $files; do
         tahoe cp tahoe:$file /tmp/$ftype/$file > /dev/null
         echo $ftype '-get: ' $counter '/' $total
         let counter=counter+1
     done
+    time2=`date +%s.%N`
+    echo $time2 - $time1 | bc > ./logs/$ftype\_totaltimeget.txt
 
     cp /tmp/time_spent_hashing ./logs/$ftype\_tshg.txt
     cp /tmp/number_of_hashops ./logs/$ftype\_nohashopsg.txt
