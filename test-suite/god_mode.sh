c9ae88338d13214e2dd59a31ac0503fb1161aad1
Eirik Haver <eiriha@tahoe03.nomansland.biz>
2010-11-03 15:49:47 +0100

    Some typos in god_mode

--- test-suite/god_mode.sh
+++ test-suite/god_mode.sh
@@ -8,7 +8,11 @@
 for i in sha256 blake  bmw  cubehash  echo  fugue  groestl  hamsi \
     jh  keccak  luffa  shabal  shavite3  simd  skein;
 do
-    /home/eiriha/tahoe-svn/scripts/do_it_all.sh $i
+    cd /home/eiriha/tahoe-svn/scripts/
+    ./do_it_all.sh $i
+    cd /home/eiriha/Tahoe-LAFS-with-SHA-3/test-suite/
     ./test_runner.sh
+    mkdir $i
+    mv log/*.txt $i/
 done
 
