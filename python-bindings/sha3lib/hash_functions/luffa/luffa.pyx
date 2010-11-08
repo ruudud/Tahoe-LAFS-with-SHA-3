e2da58714f67dfec9ee0411807324b349409d5da
Eirik Haver <eiriha@gmail.com>
2010-11-09 00:31:39 +0100

    Fixes luffa memleak

--- python-bindings/sha3lib/hash_functions/luffa/luffa.pyx
+++ python-bindings/sha3lib/hash_functions/luffa/luffa.pyx
@@ -28,11 +28,12 @@ cdef class luffa:
     so that one can update the hashing procedure instead of doing it from
     scratch.
     '''
-    cdef luffa_h.BitSequence *hashval
+    #cdef luffa_h.BitSequence *hashval
     cdef luffa_h.hashState previous_state
     cdef luffa_h.hashState state
     cdef int finished
     cdef int hashbitlen
+    cdef list hashval
 
     def __init__(self, int in_hashbitlen, bytes initial=None):
         self.finished = 0
@@ -53,14 +54,17 @@ cdef class luffa:
         luffa_h.Update(&self.state, <luffa_h.BitSequence *> data, data_len)
 
     cpdef final(self):
-        self.hashval = <luffa_h.BitSequence *> malloc(self.hashbitlen*8)
+        cdef luffa_h.BitSequence *hashval = <luffa_h.BitSequence *> malloc(self.hashbitlen*8)
 
         # We copy the state so that we can continue to update.
         # This equals hashlibs functionality, but not pycryptopp.
         self.previous_state = self.state
 
-        luffa_h.Final(&self.state, self.hashval)
+        luffa_h.Final(&self.state, hashval)
         self.finished = 1
+        
+        self.hashval = [hashval[i] for i from 0 <= i < self.hashbitlen / 8]
+        free(hashval)
 
     cpdef copy(self):
         s = luffa(self.hashbitlen)
@@ -76,14 +80,10 @@ cdef class luffa:
         if not self.finished:
             self.final()
 
-        digest = [self.hashval[i] for i from 0 <= i < self.hashbitlen / 8]
-        return ''.join(map(chr, digest))
+        return ''.join(map(chr, self.hashval))
 
     def hexdigest(self):
         if not self.finished:
             self.final()
 
-        digest = [self.hashval[i] for i from 0 <= i < self.hashbitlen / 8]
-
-        # Return a hex string using str format specification
-        return ''.join(['%02x' % i for i in digest])
+        return ''.join(['%02x' % i for i in self.hashval])
