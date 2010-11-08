f0b794101d4bcfb1e43a555aa448bf2824e3a3e0
Eirik Haver <eiriha@gmail.com>
2010-11-08 11:52:57 +0100

    Fixes to god mode

--- test-suite/god_mode.sh
+++ test-suite/god_mode.sh
@@ -5,7 +5,8 @@
 #   Starte noder osv. 
 #   Starte testing
 
-for i in sha256 blake  bmw  cubehash  echo  fugue  groestl  hamsi \
+mkdir logs
+for i in blake sha256 bmw  cubehash  echo  fugue  groestl  hamsi \
     jh  keccak  luffa  shabal  shavite3  simd  skein;
 do
     cd /home/eiriha/tahoe-svn/scripts/
@@ -13,6 +14,6 @@ do
     cd /home/eiriha/Tahoe-LAFS-with-SHA-3/test-suite/
     ./test_runner.sh
     mkdir $i
-    mv log/*.txt $i/
+    mv logs/*.txt $i/
 done
 
