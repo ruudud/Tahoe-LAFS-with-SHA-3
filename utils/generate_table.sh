db659cfaefc812dd15f666671cb503a2679b2b42
Pål Ruud <ruudud@gmail.com>
2010-12-12 15:43:05 +0100

    Moved file variables to top

--- utils/generate_table.sh
+++ utils/generate_table.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+tsh_file="1gb_tshg.txt"
+totaltime_file="1gb_totaltimeget.txt"
 for cand in `ls run-sha3-multi-4`; do
-    time_sh=`cat run-sha3-multi-7/$cand/1gb_tshp.txt`
+    time_sh=`cat run-sha3-multi-7/$cand/$tsh_file`
     numerator=0
     for r in `seq 4 7`; do
         cd run-sha3-multi-$r
-        new_num=`cat $cand/1gb_totaltimeput.txt`
+        new_num=`cat $cand/$totaltile_file`
         numerator=$(echo $numerator + $new_num | bc -q)
         cd ..
     done;
@@ -12,5 +14,6 @@ for cand in `ls run-sha3-multi-4`; do
     difference=$(echo $result - $time_sh | bc -q)
     diff_sub=${difference:0:6}
     timesh_sub=${time_sh:0:5}
-    echo -e "$cand & $result\0163 & $timesh_sub\0163 & $diff_sub\0163 \\\\\ \\hline"
+    fraction=$(echo "scale=2; $time_sh * 100 / $result" | bc -q)
+    echo -e "$cand & $result\0163 & $timesh_sub\0163 & $diff_sub\0163 & $fraction\\% \\\\\ \\hline"
 done;
